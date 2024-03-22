45db1d9056fe2e191ab6fc67adcd58054cf8054f
KoheiWatanabe <161041321+WatanabeKMF@users.noreply.github.com>
2024-03-22 14:59:46 +0000

    validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,24 +2,40 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to '/'
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to '/books'
+    else
+      flash.now[:alert] = 
+      render :new
+    end
   end
 
   def index
     @book = Book.new
 
     @books = Book.all
-
-
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
   private
